91d9a0f9758941c4073d82dfad1bdc048243de08
Alfie Ward <5115732@bournemouth.ac.uk>
2018-11-20 15:34:06 +0000

    Initial Commit

---
+++ TestProject/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  TestProject
+//
+//  Created by Alfie Ward (s5115732) on 20/11/2018.
+//  Copyright © 2018 Alfie Ward (s5115732). All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
